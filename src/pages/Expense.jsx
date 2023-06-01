55af83bab6a9fa9ec77c1a640f39229eecfa36de
victoria odemakin <victoriaodemakin@gmail.com>
2023-06-01 15:06:01 -0700

    building the expense

--- src/pages/Expense.jsx
+++ src/pages/Expense.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import TopNav from "../components/TopNav";
-import { Card } from '@mui/material';
+// import { Card } from '@mui/material';
 import Sidebar from "../components/Sidebar";
 import Table from "../components/Table";
 import WalletCard from "../components/WalletCard";
@@ -8,11 +8,11 @@ import WalletCard from "../components/WalletCard";
 // import Transaction from "../components/Transaction";
 // import CardWallet from "../components/CardWallet";
 
-import { styled } from "@mui/material/styles";
+// import { styled } from "@mui/material/styles";
 
-const StyledCard = styled(Card)`
-  display: flex;
-`
+// const StyledCard = styled(Card)`
+//   display: flex;
+// `
 
 const Expense = () => {
   return (
