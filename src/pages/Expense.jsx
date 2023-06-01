7a48c0ccce6534095543c0e231b83787b56a0a31
Theresa <theresaogu@gmail.com>
2023-06-01 16:19:00 +0100

    resolved conflicts

--- src/pages/Expense.jsx
+++ src/pages/Expense.jsx
@@ -2,10 +2,9 @@ import React from "react";
 import TopNav from "../components/TopNav";
 import { Card } from '@mui/material';
 import Sidebar from "../components/Sidebar";
-import MobileNav from "../components/MobileNav";
-import useMediaQuery from "@mui/material/useMediaQuery";
 import Table from "../components/Table";
 import WalletCard from "../components/WalletCard";
+
 // import Transaction from "../components/Transaction";
 // import CardWallet from "../components/CardWallet";
 
@@ -16,27 +15,16 @@ const StyledCard = styled(Card)`
 `
 
 const Expense = () => {
-  const isDesktop = useMediaQuery("(min-width: 1024px)");
-
   return (
     <div>
-       {isDesktop ? (
-        <div className="DesktopScreen">
-      <TopNav Title='Expenses' />
-        <Sidebar />
+      <TopNav/>
+      <Sidebar/>
       <div className="tableC">
-          <WalletCard />
-          <Table />
-        </div>
-        </div>
-       ):(
-
-        <div className="MobileScreen">
-        <MobileNav title="Expenses" />
+      <WalletCard/>
+      <Table/> 
       </div>
-    )}
-  </div>
+    </div>
   );
-}; 
+};
 
 export default Expense;
